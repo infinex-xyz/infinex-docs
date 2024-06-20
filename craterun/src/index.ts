d6c842ebeab27823134e998885e0b4a9a8951aa9
Jumbo Infinex <jumbo@core.infinex.xyz>
2024-06-20 16:10:38 +1000

    clean up unused variables and fix bugs

--- craterun/src/index.ts
+++ craterun/src/index.ts
@@ -10,15 +10,20 @@ export async function main() {
   const RandomNumberProcessedEvent =
     crateOpenerContract.filters.RandomNumberProcessed();
 
+  // Get current block number
   const END_BLOCK_NUMBER = await baseProvider.getBlockNumber();
+
+  // Generate a sequential array of crateIds, e.g. [0, 1, 2, ... n - 1]
   const crateIdArray = generateCrateIds();
 
+  // Fetch all RandomNumberProcessed events between START_BLOCK_NUMBER and END_BLOCK_NUMBER
   const events = await fetchEvents(
     RandomNumberProcessedEvent,
     START_BLOCK_NUMBER,
     END_BLOCK_NUMBER
   );
 
+  // Calculate user rewards
   return calculateRewards(events, crateIdArray);
 }
 
